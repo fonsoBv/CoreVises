e04eb873cf1d98ad2871d8cf29bcc1260907797f
fonsoBv <31110577+fonsoBv@users.noreply.github.com>
2017-08-26 10:44:51 -0600

    fonso

---
+++ MongoDB/WcfTienda/IService1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfTienda
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int insertAdmi(string user, string password);
+
+        [OperationContract]
+        int updateAdmi(string user, string password);
+
+        [OperationContract]
+        int deleteAdmi(string user);
+
+        [OperationContract]
+        int initSession(string user, string password);
+
+    }
+}
